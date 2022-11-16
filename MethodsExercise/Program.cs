30e3af770f68a676a9d345749467fab2ab014930
Chanise Hart <niecy23@gmail.com>
2022-11-16 12:38:09 -0700

    Part 1 of Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
-    {
+    { 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("What is your name?");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string FavoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string FavoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string FavoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"{Name} had a dream that a {FavoriteColor} {FavoriteAnimal} was playing {FavoriteBand} at Coachella.");
+
         }
     }
 }
